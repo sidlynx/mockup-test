e1131b5c2e9e0be3d6e6cfa3ef85c54f68bea3a8
Mohammed Hamdoune <sidlynx@gmail.com>
2018-12-11 00:40:55 +0100

    home page

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="home">
-    <h1>Home page</h1>
+    <h1>Christmas time !</h1>
   </div>
 </template>
+<script>
+import Page from "@/components/layout/page.vue";
+export default {
+  components: {
+    Page
+  }
+};
+</script>
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    font-size: 140px;
+    font-weight: bold;
+    color: #fff;
+  }
+}
+</style>
