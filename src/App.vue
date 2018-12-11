203803a773af1aaf1649fd99db287ebc73861282
Mohammed Hamdoune <sidlynx@gmail.com>
2018-12-11 11:18:31 +0100

    enhance menu styles

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,77 @@
 <template>
   <div id="app">
-    <div id="menu">
-      <div class="icon-holder">
-        <img src="/public/icons/menu.svg" />
-      </div>
-      <router-link
-        tag="div"
-        :to="{name:'home'}"
-        class="icon-holder"
-      >
-        <img src="/public/icons/snow-globe.svg" />
-      </router-link>
-      <router-link
-        tag="div"
-        :to="{name:'about'}"
-        class="icon-holder"
-      >
-        <img src="/public/icons/icon.svg" />
-      </router-link>
-      <router-link
-        tag="div"
-        :to="{name:'contact'}"
-        class="icon-holder"
-      >
-        <img src="/public/icons/auricular-phone-symbol-in-a-circle.svg" />
-      </router-link>
-      <router-link
-        tag="div"
-        :to="{name:'pricing'}"
-        class="icon-holder"
-      >
-        <img src="/public/icons/coin.svg" />
-      </router-link>
+    <div
+      id="menu"
+      :style="{width : menu.expanded ? '400px':'120px'}"
+    >
+      <section class="section-menu">
+        <div class="icon-holder">
+          <img
+            src="/public/icons/menu.svg"
+            @click="menu.expanded = !menu.expanded"
+          />
+        </div>
+      </section>
+      <section class="section-home">
+        <router-link
+          tag="div"
+          :to="{name:'home'}"
+          class="icon-holder"
+        >
+          <img
+            src="/public/icons/snow-globe.svg"
+            v-if="!menu.expanded"
+          />
+          <div
+            class="title"
+            v-if="menu.expanded"
+          >Holy<br /> Joly<br /> Christmas</div>
+        </router-link>
+      </section>
+      <section class="section-items">
+        <router-link
+          tag="div"
+          :to="{name:'about'}"
+          class="icon-holder"
+        >
+          <img
+            src="/public/icons/icon.svg"
+            v-if="!menu.expanded"
+          />
+          <div
+            class="title"
+            v-if="menu.expanded"
+          >About us</div>
+        </router-link>
+        <router-link
+          tag="div"
+          :to="{name:'contact'}"
+          class="icon-holder"
+        >
+          <img
+            src="/public/icons/auricular-phone-symbol-in-a-circle.svg"
+            v-if="!menu.expanded"
+          />
+          <div
+            class="title"
+            v-if="menu.expanded"
+          >Contact</div>
+        </router-link>
+        <router-link
+          tag="div"
+          :to="{name:'pricing'}"
+          class="icon-holder"
+        >
+          <img
+            src="/public/icons/coin.svg"
+            v-if="!menu.expanded"
+          />
+          <div
+            class="title"
+            v-if="menu.expanded"
+          >Pricing</div>
+        </router-link>
+      </section>
     </div>
     <div id="content">
       <router-view />
@@ -40,6 +80,13 @@
 </template>
 <script>
 export default {
+  data() {
+    return {
+      menu: {
+        expanded: false
+      }
+    };
+  },
   components: {}
 };
 </script>
@@ -59,13 +106,44 @@ export default {
     top: 0px;
     left: 0px;
     background-color: #7d0d0b;
-    opacity: 0.7;
-    .icon-holder {
-      display: flex;
-      width: 100%;
-      height: 50px;
-      justify-content: center;
-      cursor: pointer;
+    opacity: 0.9;
+    display: flex;
+    flex-direction: column;
+    z-index: 2;
+    section {
+      .icon-holder {
+        width: 100%;
+        height: 50px;
+        padding-left: 30px;
+        cursor: pointer;
+        img {
+          width: 50px;
+          height: 50px;
+        }
+        div {
+          height: 50px;
+          color: white;
+          font-size: 60px;
+          font-weight: bold;
+        }
+        margin-bottom: 50px;
+      }
+
+      &.section-menu {
+        flex-basis: 15%;
+      }
+      &.section-home {
+        flex-basis: 35%;
+        .icon-holder {
+          div {
+            font-size: 70px;
+          }
+        }
+      }
+
+      &.section-items {
+        flex-basis: 50%;
+      }
     }
   }
   #content {
